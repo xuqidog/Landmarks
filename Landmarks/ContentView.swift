0f69d996981f94ca4c00f3d9c640006e80c80513
theme <1005071865@qq.com>
2019-11-20 19:39:08 +0800

    Initial commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 徐其东 on 2019/11/20.
+//  Copyright © 2019 xuqidong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
